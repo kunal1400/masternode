ae2a9fcd3840ad1808ccb3882f198a9081ed17ca
Kunal Malviya <kunal.malviya351@gmail.com>
2019-04-22 16:41:12 +0530

    Implemented the formulaes

--- masternode.php
+++ masternode.php
@@ -41,10 +41,13 @@ function mno_callback( $atts ) {
 	// Get shortcodes
 	$a = shortcode_atts( array(
 		"coin_ticker" => "",
-		"get" => ""
+		"get" => "",
+		"formula" => 1
 	), $atts );
 	
 	$output = "";
+	$coinValue = 0;
+	$selectedCoin = array();
 
 	if( empty($a['get']) ) {
 		$output = "get parameter is empty";
@@ -52,7 +55,7 @@ function mno_callback( $atts ) {
 
 	if( !empty($a['coin_ticker']) ) {
 		// Response 
-		$response = wp_remote_get( "https://masternodes.online/mno_api/?apiseed=MNOAPI-0063-ac44ae4a-5c47ac12-50a3-5cfc6053" );
+		$response = wp_remote_get( "https://masternodes.online/mno_api/?apiseed=MNOAPI-0063-ac44ae4a-5c47ac12-50a3-5cfc6053" );				
 
 		if ( is_array( $response ) && ! is_wp_error( $response ) ) {
 		    $headers = $response['headers']; // array of http header lines
@@ -61,23 +64,56 @@ function mno_callback( $atts ) {
 		    	$coins = json_decode($body, ARRAY_A);
 		    	foreach ($coins as $i => $coin) {
 		    		if( @$coin['coin_ticker'] == @$a['coin_ticker'] ) {
-		    			$output = @$coin[$a['get']];		    			
+		    			$coinValue = @$coin[$a['get']];
+		    			$selectedCoin = $coin;
 		    		}		    		
 		    	}		  
-		    }	    
-		}		
+		    }
+		}
+		else {
+			$output = "Some error occured while calling the api";
+			// echo '<pre>';
+			// print_r($response);
+			// echo '</pre>';			
+		}
 	}
 	else {
 		$output = "Coin name is required";
 	}
-	
-	// // Daily income usd
-	if( isset($a['get']) &&  $a['get'] == 'daily_income_usd' ) {
-		return '<div id="masterNodeWrapper">
+
+	if( $a['formula'] == 1 ) {
+		$output = ($coinValue*30)-9.99;
+		$output = round($output, 1);
+	}	
+	else if( $a['formula'] == 2 ) {
+		$output = $coinValue*7;
+		$output = round($output, 1);		
+	}
+	else if( $a['formula'] == 3 ) {
+		$output = $coinValue*30;
+		$output = round($output, 1);		
+	}
+	else if( $a['formula'] == 4 ) {
+		$output = $coinValue*365;
+		$output = round($output, 1);		
+	}
+	else if( $a['formula'] == 5 ) {
+		// echo '<pre>';
+		// print_r($output);
+		// echo '</pre>';
+		$output = $selectedCoin['daily_income_usd']*365*100/(($selectedCoin['price_usd']*$selectedCoin['required_coins_for_masternode'])+(9.99*12));
+		$output = round($output, 1);		
+	}
+	else if( $a['formula'] == 6 ) {
+		$output = $selectedCoin['daily_income_usd']*365*100/(($selectedCoin['price_usd']*$selectedCoin['required_coins_for_masternode'])+(29.99*12));
+		$output = round($output, 1);		
+	}
+	else if( $a['formula'] == 7 ) {
+		$output = '<div id="masterNodeWrapper">
 			<table>
 				<thead>
 					<tr>
-						<th>'.$a['coin_ticker'].' - Daily Income USD</th>
+						<th>'.$a['coin_ticker']. $a['get'] .'</th>
 						<th>Input</th>
 						<th>Action</th>
 						<th>Output</th>
@@ -85,7 +121,7 @@ function mno_callback( $atts ) {
 				</thead>
 				<tbody>
 					<tr>
-						<td>'.$output.'</td>
+						<td>'.$coinValue.'</td>
 						<td><input type="number" id="masterNodeNumberInput" /></td>
 						<td><button onclick="doCalculations()" id="masterNodeNumberButton">Submit</button></td>
 						<td>
@@ -99,13 +135,10 @@ function mno_callback( $atts ) {
 		<script>
 			function doCalculations() {
 				var _input = document.getElementById("masterNodeNumberInput").value;
-				var _coinVal = '.$output.';
+				var _coinVal = '.$coinValue.';
 				if(_input) {
-					var formula1 = _input * _coinVal * 30 * 0.01;
 					var formula2 = (_input - _coinVal * 30) - 9.9;
-					document.getElementById("masterNodeOutput1").value = formula1.toFixed(1)
 					document.getElementById("masterNodeOutput2").value = formula2.toFixed(1)
-					//alert(calculation)
 				}
 				else {
 					alert("Input is empty")					
@@ -113,6 +146,51 @@ function mno_callback( $atts ) {
 			}
 		</script>';
 	}	
+	else if( $a['formula'] == 8 ) {
+		$output = '<div id="masterNodeWrapper">
+			<table>
+				<thead>
+					<tr>
+						<th>'.$a['coin_ticker']. $a['get'] .'</th>
+						<th>Input</th>
+						<th>Action</th>
+						<th>Output</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>'.$coinValue.'</td>
+						<td><input type="number" id="masterNodeNumberInput" /></td>
+						<td><button onclick="doCalculations()" id="masterNodeNumberButton">Submit</button></td>
+						<td>
+							<input type="number" disabled="disabled" id="masterNodeOutput1" />
+							<input type="number" disabled="disabled" id="masterNodeOutput2" />
+						</td>
+					</tr>
+				</tbody>				
+			</table>
+		</div>
+		<script>
+			function doCalculations() {
+				var _input = document.getElementById("masterNodeNumberInput").value;
+				var _coinVal = '.$coinValue.';
+				if(_input) {
+					var formula2 = _input*_coinVal*30*0.01;
+					document.getElementById("masterNodeOutput2").value = formula2.toFixed(1)
+				}
+				else {
+					alert("Input is empty")					
+				}
+			}
+		</script>';
+	}
+	else {
+		$output = $coinValue;
+	}
+
+	// // // Daily income usd
+	// if( isset($a['get']) &&  $a['formula'] == 2 ) {
+	// 	return 	}
 
 	return $output;
 }
